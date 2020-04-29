04d19b93acdbeb3cf14bf754ab75003ec1f81490
Ltyro <17816874829@163.com>
2020-04-29 17:09:07 +0800

    schedule complete finally.

--- src/main/java/lnstark/schedule/MySchedule.java
+++ src/main/java/lnstark/schedule/MySchedule.java
@@ -17,28 +17,10 @@ import java.util.TimerTask;
 @Component
 public class MySchedule {
 
-    @Scheduled(cron="* * 17 28 4 *")
+    @Scheduled(cron="20-40/5 29 * * 4 2,4")
     public void testSchedule() {
         System.out.println("dida");
     }
 
-    public static void main(String[] args) {
-        Timer t = new Timer();
-        Calendar c = Calendar.getInstance();
-        c.set(Calendar.SECOND, 0);
-        c.set(Calendar.MINUTE, 35);
-        c.set(Calendar.HOUR_OF_DAY, 22);
-        c.set(Calendar.DATE, 21);
-        c.set(Calendar.DAY_OF_WEEK, 3);
-        t.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                System.out.println("hello");
-                t.cancel();
-            }
-        }, c.getTime());
-
-        System.out.println("after setting schedule");
-    }
 
 }
--- src/main/java/lnstark/schedule/core/TaskScheduler.java
+++ src/main/java/lnstark/schedule/core/TaskScheduler.java
@@ -1,25 +1,23 @@
 package lnstark.schedule.core;
 
-import lnstark.exception.ScheduleException;
-import lnstark.schedule.annos.Scheduled;
-import lnstark.schedule.entity.Time;
-import lnstark.schedule.entity.TimeType;
-import lnstark.utils.StringUtil;
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.BitSet;
 import java.util.Calendar;
 import java.util.Timer;
-import java.util.TimerTask;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+
+import lnstark.exception.ScheduleException;
+import lnstark.schedule.annos.Scheduled;
+import lnstark.schedule.entity.Time;
+import lnstark.utils.StringUtil;
 
 /**
  * A CRON expression parser and task executor
  * Reference from #{CronSequenceGenerator}
  */
-public class TaskScheduler implements Runnable{
+public class TaskScheduler implements Runnable {
 
     private BitSet seconds      = new BitSet(60);
 
@@ -39,10 +37,12 @@ public class TaskScheduler implements Runnable{
     
     private Object target;
     
-    private Scheduled schedule;
+//    private Scheduled schedule;
     
-    private boolean start = false, end = false;
+//    private boolean start = false, end = false;
 
+//    private Calendar lastExecutionTime = null;
+    
     private ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(3);
 
     private static final String ERROR_EXPRESSION = "error cron expression";
@@ -60,15 +60,12 @@ public class TaskScheduler implements Runnable{
         };
         this.method = m;
         this.target = o;
-        this.schedule = sa;
+//        this.schedule = sa;
         
         String cron = sa.cron();
         if (StringUtil.isEmpty(cron))
             throw new NullPointerException("cron expression should not be null");
-     // int sec = 0, min = 0, hour = 0, day = 0, month = 0, weekday = 0;
-//      Time sec = new Time(0, false, TimeType.Second), min = new Time(0, false, TimeType.Minute),
-//              hour = new Time(0, false, TimeType.Hour), day = new Time(1, false, TimeType.Day),
-//              month = new Time(0, false, TimeType.Month), weekday = new Time(0, false, TimeType.Week);
+        
         String[] times = cron.split(" ");
         if (times.length != 6)
             throw new ScheduleException(ERROR_EXPRESSION);
@@ -80,20 +77,12 @@ public class TaskScheduler implements Runnable{
 
     public void execute() {
         
-        // execute first time
         long delay = 0;
         Calendar c = Calendar.getInstance();
-        if (!start) {
-        	long firstTime = getFirstTimeFromBitSets(Calendar.getInstance());
-        	delay = firstTime - System.currentTimeMillis();
-        	if(delay < 0)
-        		delay = getNextTime(c) - System.currentTimeMillis();
-        	start = true;
-        } else {
-        	delay = getNextTime(c) - System.currentTimeMillis();
-        }
-        if (end)
-        	return;
+        c.add(Calendar.SECOND, 1);
+        // while next execution time is earlier than current time, do getNextTime
+        while((delay = getNextTime(c) - System.currentTimeMillis()) <= 0);
+        System.out.println("next execution time is: " + c.getTime());
         executor.schedule(this, delay, TimeUnit.MILLISECONDS);
     }
 
@@ -107,14 +96,6 @@ public class TaskScheduler implements Runnable{
         if (StringUtil.isNumberOnly(time)) {
             int value = getTimeValue(time, type);
             timeField.set(value);
-            return;
-        }
-
-        if (time.contains("-")) {
-            String[] strs = time.split("-");
-            int start = getTimeValue(strs[0], type),
-                    end = getTimeValue(strs[1], type);
-            setPeriodTimeField(timeField, start, end, 1, timeLength);
             return;
         }
 
@@ -138,7 +119,11 @@ public class TaskScheduler implements Runnable{
 
             int period = Integer.parseInt(strs[1]);
             setPeriodTimeField(timeField, start, end, period, timeLength);
-            return;
+        } else if (time.contains("-")) {
+            String[] strs = time.split("-");
+            int start = getTimeValue(strs[0], type),
+                    end = getTimeValue(strs[1], type);
+            setPeriodTimeField(timeField, start, end, 1, timeLength);
         }
 
     }
@@ -161,7 +146,7 @@ public class TaskScheduler implements Runnable{
 
     private int getTimeValue(String time, int type) {
         int value = Integer.parseInt(time);
-        if (type == Calendar.DAY_OF_MONTH)
+        if (type == Calendar.DAY_OF_MONTH || type == Calendar.MONTH)
             value--;
         if (type == Calendar.DAY_OF_WEEK && value == 7)
             value = 0;
@@ -174,80 +159,7 @@ public class TaskScheduler implements Runnable{
      */
     private long getNextTime(Calendar c) {
     	c.set(Calendar.MILLISECOND, 0);
-    	
-    	// second
-    	int sec = c.get(Calendar.SECOND);
-        int nextSec = seconds.nextSetBit(sec + 1);
-        if (nextSec == -1) {
-        	nextSec = seconds.nextSetBit(0);
-            c.set(Calendar.SECOND, nextSec);
-        } else {
-        	c.set(Calendar.SECOND, nextSec);
-        	return c.getTimeInMillis();
-        }
-        
-        // minute
-        int min = c.get(Calendar.MINUTE);
-        int nextMin = minutes.nextSetBit(min + 1);
-        if (nextMin == -1) {
-        	nextMin = minutes.nextSetBit(0);
-            c.set(Calendar.MINUTE, nextMin);
-        } else {
-        	c.set(Calendar.MINUTE, nextMin);
-        	return c.getTimeInMillis();
-        }
-        
-        // hour
-        int h = c.get(Calendar.HOUR);
-        int nextH = minutes.nextSetBit(h + 1);
-        if (nextH == -1) {
-        	nextH = minutes.nextSetBit(0);
-            c.set(Calendar.HOUR, nextH);
-        } else {
-        	c.set(Calendar.HOUR, nextH);
-        	return c.getTimeInMillis();
-        }
-
-        nextDay(c);
-
-        return c.getTimeInMillis();
-    }
-
-    // day
-    private void nextDay(Calendar c) {
-        int d = c.get(Calendar.DAY_OF_MONTH);
-        int nextDay = daysOfMonth.nextSetBit(d);
-        
-        if (nextDay == -1) {
-            nextDay = daysOfMonth.nextSetBit(0);
-            c.set(Calendar.DAY_OF_MONTH, nextDay);
-            nextMonth(c);
-        }
-        
-    	if (!fitDayOfWeek(c))
-        	nextDay(c);
-    }
-
-    // month
-    private void nextMonth(Calendar c) {
-
-        int mon = c.get(Calendar.MONTH);
-        int nextMon = daysOfMonth.nextSetBit(mon + 1);
-        if (nextMon == -1) {
-            c.add(Calendar.YEAR, 1);// next year
-        	nextMon = daysOfMonth.nextSetBit(0);
-        }
-        c.set(Calendar.MONTH, nextMon);
-
-    }
-
-    private boolean fitDayOfWeek(Calendar c) {
-        int dow = c.get(Calendar.DAY_OF_WEEK);
-        return daysOfWeek.get(dow - 1);
-    }
-
-    private long getFirstTimeFromBitSets(Calendar c) {
-        c.set(Calendar.MILLISECOND, 0);
+    	boolean resetSec = false, resetMin = false, resetHour = false;
         int second = c.get(Calendar.SECOND);
         while (!seconds.get(second)) {
         	c.add(Calendar.SECOND, 1);
@@ -258,66 +170,66 @@ public class TaskScheduler implements Runnable{
         while (!minutes.get(min)) {
         	c.add(Calendar.MINUTE, 1);
         	min = c.get(Calendar.MINUTE);
+        	if (!resetSec) {
+	        	c.set(Calendar.SECOND, seconds.nextSetBit(0));
+	        	resetSec = true;
+        	}
         }
         
         int h = c.get(Calendar.HOUR_OF_DAY);
         while (!hours.get(h)) {
         	c.add(Calendar.HOUR_OF_DAY, 1);
         	h = c.get(Calendar.HOUR_OF_DAY);
+        	if (!resetMin) {
+        		c.set(Calendar.MINUTE, minutes.nextSetBit(0));
+        		resetMin = true;
+        	}
         }
-        
-        int d = c.get(Calendar.DAY_OF_MONTH);
-        
-//        c.set(Calendar.MINUTE, minutes.nextSetBit(0));
-//        c.set(Calendar.HOUR_OF_DAY, hours.nextSetBit(0));
-//        c.set(Calendar.DAY_OF_MONTH, daysOfMonth.nextSetBit(0) + 1);
-//        c.set(Calendar.MONTH, months.nextSetBit(0));
-//        c.set(Calendar.DAY_OF_WEEK, daysOfWeek.nextSetBit(0));
+
+        nextDay(c);
+
         return c.getTimeInMillis();
     }
 
-    
-    private void executeSchedule(Time[] timeArr) {
-        if (timeArr == null || timeArr.length != 6)
-            return;
-        boolean once = true, // 只执行一次
-                always = true;
-
-        for (int i = 0; i < timeArr.length; i++) {
-            if (timeArr[i].isEvery()) {
-                always = false;
-                once = false;
-                break;
-            }
+    // day
+    private void nextDay(Calendar c) {
+    	boolean resetHour = false;
+        
+        while (!daysOfMonth.get(c.get(Calendar.DAY_OF_MONTH) - 1) ||
+        		!months.get(c.get(Calendar.MONTH)) ||
+        		!daysOfWeek.get(c.get(Calendar.DAY_OF_WEEK) - 1)) {
+        	c.add(Calendar.DAY_OF_MONTH, 1);
+        	if (!resetHour) {
+        		c.set(Calendar.HOUR_OF_DAY, hours.nextSetBit(0));
+        		resetHour = true;
+        	}
+//            nextMonth(c);
         }
+        
+//    	if (!fitDayOfWeek(c))
+//        	nextDay(c);
+    }
 
-        Timer t = new Timer();
-        Calendar c = Calendar.getInstance();
-        c.set(Calendar.MILLISECOND, 0);
+    // month
+    private void nextMonth(Calendar c) {
 
-        if (!timeArr[0].isEvery())
-            c.set(Calendar.SECOND, timeArr[0].getValue());
-        if (!timeArr[1].isEvery())
-            c.set(Calendar.MINUTE, timeArr[1].getValue());
-        if (!timeArr[2].isEvery())
-            c.set(Calendar.HOUR_OF_DAY, timeArr[2].getValue());
-        if (!timeArr[3].isEvery())
-            c.set(Calendar.DATE, timeArr[3].getValue());
-        if (!timeArr[4].isEvery())
-            c.set(Calendar.MONTH, timeArr[4].getValue() - 1);
-        if (!timeArr[5].isEvery())
-            c.set(Calendar.DAY_OF_WEEK, (timeArr[5].getValue() + 1) % 7);
+        int mon = c.get(Calendar.MONTH);
+        int nextMon = months.nextSetBit(mon + 1);
+        if (nextMon == -1) {
+            c.add(Calendar.YEAR, 1);// next year
+        	nextMon = months.nextSetBit(0);
+        }
+        c.set(Calendar.MONTH, nextMon);
 
-        System.out.println(c.getTime());
+    }
 
-//        t.schedule(new TimerTask() {
-//            @Override
-//            public void run() {
-//            invokeMethod(m, o);
-//            t.cancel();
-//            }
-//        }, c.getTime());
-
+    private boolean fitMonth(Calendar c) {
+        return months.get(c.get(Calendar.MONTH) - 1);
+    }
+    
+    private boolean fitDayOfWeek(Calendar c) {
+        int dow = c.get(Calendar.DAY_OF_WEEK);
+        return daysOfWeek.get(dow - 1);
     }
 
     private void invokeMethod() {
